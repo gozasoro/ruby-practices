defab6fe1c0cc2a78f4f615ad7d78efe109b8864
gozasoro <62997922+gozasoro@users.noreply.github.com>
2021-01-09 17:07:51 +0900

    レビューいただいた部分を修正

--- 07.bowling_object/bowling.rb
+++ 07.bowling_object/bowling.rb
@@ -2,56 +2,42 @@
 # frozen_string_literal: true
 
 class Game
-  def initialize(result)
-    @result = result
+  def initialize(shot_text)
+    @shot_text = shot_text
   end
 
-  def show_score
-    shots = create_shots(@result)
+  def score
+    shots = create_shots(@shot_text)
     frames = create_frames(shots)
-    puts frames.map(&:score).inject(:+)
+    frames.map(&:score).sum
   end
 
   private
 
-  def create_shots(result)
-    result.chars.map { |mark| Shot.new(mark) }
+  def create_shots(shot_text)
+    shot_text.chars.map { |mark| Shot.new(mark) }
   end
 
   def create_frames(shots)
-    (1..10).each_with_object([]) do |count, frames|
+    (1..10).map do |n|
       first_shot = shots.shift
-      if count == 10
+      if n == 10
         second_shot = shots.shift
         third_shot = shots.shift
       elsif first_shot.score != 10
         second_shot = shots.shift
       end
-      frames.push create_frame(first_shot, second_shot, third_shot, shots)
+      Frame.new(first_shot, second_shot, third_shot, shots)
     end
-  end
-
-  def create_frame(first_shot, second_shot, third_shot, shots)
-    frame = Frame.new(first_shot, second_shot, third_shot)
-    return frame if shots.empty?
-
-    if frame.strike?
-      frame.bonus_score = shots[0].score + shots[1].score
-    elsif frame.spare?
-      frame.bonus_score = shots[0].score
-    end
-    frame
   end
 end
 
 class Frame
-  attr_accessor :bonus_score
-
-  def initialize(first_shot, second_shot, third_shot)
+  def initialize(first_shot, second_shot, third_shot, shots)
     @first_shot = first_shot
     @second_shot = second_shot
     @third_shot = third_shot
-    @bonus_score = 0
+    @bonus_score = calculate_bonus_score(shots)
   end
 
   def score
@@ -67,7 +53,21 @@ class Frame
   end
 
   def spare?
-    @first_shot.score + @second_shot.score == 10
+    !strike? && @first_shot.score + @second_shot.score == 10
+  end
+
+  private
+
+  def calculate_bonus_score(shots)
+    return 0 if shots.empty?
+
+    bonus_score = 0
+    if strike?
+      bonus_score = shots[0].score + shots[1].score
+    elsif spare?
+      bonus_score = shots[0].score
+    end
+    bonus_score
   end
 end
 
@@ -77,10 +77,8 @@ class Shot
   end
 
   def score
-    return 10 if @mark == 'X'
-
-    @mark.to_i
+    @mark == 'X' ? 10 : @mark.to_i
   end
 end
 
-Game.new(ARGV[0]).show_score
+puts Game.new(ARGV[0]).score
