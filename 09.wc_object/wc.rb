b0018e1ccab341af216815a591e6c45fbc0f243e
gozasoro <62997922+gozasoro@users.noreply.github.com>
2021-01-10 04:49:31 +0900

    レビューいただいた部分を修正

--- 09.wc_object/wc.rb
+++ 09.wc_object/wc.rb
@@ -5,93 +5,101 @@ require 'optparse'
 
 module WC
   class Command
-    def initialize(argv)
-      @argv = argv
+    def initialize(options_and_files)
+      @options_and_files = options_and_files
+      @show_options = {}
     end
 
     def call
       opt = OptionParser.new
 
-      options = {}
-      opt.on('-l') { |v| options[:l] = v }
-      opt.parse!(@argv)
+      opt.on('-l') { |v| @show_options[:l] = v }
+      opt.parse!(@options_and_files)
 
-      files = @argv.empty? ? create_stdin_file : create_files(@argv)
-      show(files, options)
+      files = @options_and_files.empty? ? create_stdin_file : create_files(@options_and_files)
+      puts show(files)
     end
 
     private
 
+    def line_only?
+      @show_options[:l]
+    end
+
     def create_stdin_file
-      [WC::StdinFile.new(readlines)]
+      [StdinFileInfo.new(readlines)]
     end
 
     def create_files(argv)
-      filenames = argv.each_with_object([]) { |arg, file_args| file_args.concat(Dir.glob(arg)) }
-      filenames.map { |filename| WC::File.new(filename) }
+      filenames = argv.map { |arg| Dir.glob(arg) }.flatten
+      filenames.map { |filename| FileInfo.new(filename) }
     end
 
-    def show(files, options)
-      width = calculate_width(files, options)
-      display_lines(files, options, width)
-      display_total(files, options, width) if files.size > 1
+    def show(files)
+      width = calculate_width(files)
+      create_file_info_strings(files, width)
     end
 
-    def display_lines(files, options, width)
-      files.each do |f|
-        line = [f.lineno, f.name]
-        line.insert(1, f.wordno, f.size) unless options[:l]
-        puts line.map { |l| l.to_s.rjust(width) }.join(' ')
-      end
+    def create_file_info_strings(files, width)
+      rows = files.map { |file| line_only? ? [file.line_count, file.name] : [file.line_count, file.word_count, file.size, file.name] }
+      rows.push create_total_line(files) if files.size > 1
+      rows.map do |columns|
+        columns.map { |column| column.to_s.rjust(width) }.join(' ')
+      end.join("\n")
     end
 
-    def display_total(files, options, width)
-      line = [files.sum(&:lineno), 'total']
-      line.insert(1, files.sum(&:wordno), files.sum(&:size)) unless options[:l]
-      puts line.map { |l| l.to_s.rjust(width) }.join(' ')
+    def create_total_line(files)
+      line_only? ? [files.sum(&:lineno), 'total'] : [files.sum(&:line_count), files.sum(&:word_count), files.sum(&:size), 'total']
     end
 
-    def calculate_width(files, options)
-      return 7 if files.first.is_a?(WC::StdinFile)
+    def calculate_width(files)
+      return 7 if files.first.is_a?(StdinFileInfo)
 
-      if options[:l]
-        files.map(&:lineno).max.to_s.length
+      if line_only?
+        files.map(&:line_count).max.to_s.length
       else
         files.map(&:size).max.to_s.length
       end
     end
   end
 
-  class StdinFile
-    attr_reader :size, :name, :lineno
+  class StdinFileInfo
+    attr_reader :size, :name, :line_count, :word_count
 
     def initialize(lines)
-      @lines = lines
       @size = lines.map(&:bytesize).sum
       @name = ''
-      @lineno = lines.size
-    end
-
-    def wordno
-      @lines.join(' ').split(/\s+/).size
+      @line_count = lines.size
+      @word_count = FileFunction.count_words(lines)
     end
   end
 
-  class File
-    attr_reader :size, :name
+  class FileInfo
+    attr_reader :size, :name, :line_count, :word_count
 
     def initialize(filename)
-      @filename = filename
-      @name = ::File.basename(filename)
-      @size = ::File.lstat(filename).size
+      @name = File.basename(filename)
+      @size = File.lstat(filename).size
+      @line_count, @word_count = count_lines_and_words(filename)
     end
 
-    def lineno
-      ::File.open(@filename) { |f| f.readlines.count }
-    end
+    private
 
-    def wordno
-      ::File.open(@filename) { |f| f.readlines.join(' ').split(/\s+/).size }
+    def count_lines_and_words(filename)
+      line_count = 0
+      word_count = 0
+      File.open(filename) do |file|
+        lines = file.readlines
+        line_count = lines.count
+        word_count = FileFunction.count_words(lines)
+      end
+      [line_count, word_count]
+    end
+  end
+
+  module FileFunction
+    def self.count_words(lines)
+      lines.join(' ').split(/\s+/).size
     end
   end
 end
